7a6efe9b28f796ded493f4e36d5142024b92bb5a
sheel <sheel10x@gmail.com>
2026-07-19 17:41:21 +0530

    Remove invalid NextConfig property

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   devIndicators: false as any,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
